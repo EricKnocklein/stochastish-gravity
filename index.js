dbb960d5df2fe88f3510ceb2ccad54da4347aec5
eknocklein <eric.knocklein@gmail.com>
2025-02-26 20:30:33 -0500

    refactor index

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
 import Space from "./classes/space.js";
 
-const gradientSim = document.getElementById('gradient');
-
 const noBias = () => {
   return Math.random();
-}
+};
 const lowBias = () => {
   return Math.random() ** 2;
-}
+};
 const highBias = () => {
   return 1 - Math.random() ** 2;
-}
+};
 const midBias = () => {
   return (Math.random() + Math.random()) / 2;
-}
+};
 const outBias = () => {
   let x = Math.random();
-  return x < 0.5 ? x * x * 2 : 1 - ((1 - x) * (1 - x) * 2);
-}
+  return x < 0.5 ? x * x * 2 : 1 - (1 - x) * (1 - x) * 2;
+};
 
+const setUpGradientSim = () => {
+  const gradientSim = document.getElementById("gradient");
 
-const gradientSelector = () => {
-  const x = highBias();
-  const y = outBias();
+  const gradientSelector = () => {
+    const x = highBias();
+    const y = outBias();
 
-  return {x: x, y: y};
-}
+    return { x: x, y: y };
+  };
 
-const space = new Space(gradientSim, 60, 30, gradientSelector, 10);
+  const space = new Space(gradientSim, 60, 30, gradientSelector, 10);
 
-for (let i = 0; i < 30; i++) {
-  for (let j = 0; j < 14; j++) {
-    const x = (i * 10) + 150;
-    const y = (j * 10) + 80;
-    space.addParticle(x, y);
+  for (let i = 0; i < 30; i++) {
+    for (let j = 0; j < 14; j++) {
+      const x = i * 10 + 150;
+      const y = j * 10 + 80;
+      space.addParticle(x, y);
+    }
   }
-}
 
-const originalCneter = space.center.cloneNode();
-originalCneter.classList.add('original');
-space.particleHolder.appendChild(originalCneter);
-window.particles = space.particles;
+  const originalCneter = space.center.cloneNode();
+  originalCneter.classList.add("original");
+  space.particleHolder.appendChild(originalCneter);
+  window.particles = space.particles;
 
-let runSim = false;
-const runner = () => {
-  if (!runSim) {
-    return;
-  }
-  for (let i = 0; i < 150; i++) {
-    space.update();
-  }
-  window.requestAnimationFrame(() => {
+  let runSim = false;
+  const runner = () => {
+    if (!runSim) {
+      return;
+    }
+    for (let i = 0; i < 150; i++) {
+      space.update();
+    }
+    window.requestAnimationFrame(() => {
+      runner();
+    });
+  };
+
+  gradientSim.addEventListener("click", () => {
+    runSim = !runSim;
     runner();
-  })
-}
+  });
+};
 
-gradientSim.addEventListener('click', () => {
-  runSim = !runSim;
-  runner();
-})
+setUpGradientSim();
