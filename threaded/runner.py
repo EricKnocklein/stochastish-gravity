e93b6c8c6c8f38ef9094113d03c5662163991d04
eknocklein <eric.knocklein@gmail.com>
2025-09-21 17:20:24 -0400

    standard test working

--- threaded/runner.py
+++ threaded/runner.py
@@ -4,9 +4,6 @@ from plotter import InteractiveScatter
 import math
 import threading
 
-window = pyglet.window.Window(1200, 800)
-space = Space(window.width, window.height)
-
 def create_particles(space: Space, x_num, y_num, x_sep, y_sep):
   for i in range(x_num):
     for j in range(y_num):
@@ -35,52 +32,58 @@ def spawn_flower_particles(n, angle=137.5, spacing=5, outward_step=0.5):
 
   return particles
 
-# create_particles(space, 15, 10, 20, 20)
-flower_particles = spawn_flower_particles(150, spacing=9, outward_step=0.01)
-for x, y in flower_particles:
-  space.addParticle(x + window.width / 2, y + window.height / 2)
+def run(t_num):
+  window = pyglet.window.Window(1200, 800)
+  space = Space(window.width, window.height)
+  # create_particles(space, 15, 10, 20, 20)
+  flower_particles = spawn_flower_particles(150, spacing=9, outward_step=0.01)
+  for x, y in flower_particles:
+    space.addParticle(x + window.width / 2, y + window.height / 2)
 
-space.centerParticles()
+  space.centerParticles()
 
-batch = pyglet.graphics.Batch()
+  batch = pyglet.graphics.Batch()
 
-@window.event
-def on_draw():
-  window.clear()
-  space.draw(batch, circleFunction=pyglet.shapes.Circle)
-  # Drawing code would go here
-  batch.draw()
+  @window.event
+  def on_draw():
+    window.clear()
+    space.draw(batch, circleFunction=pyglet.shapes.Circle)
+    # Drawing code would go here
+    batch.draw()
 
-def stop_after_time(_dt):
-  if space.numParticlesUpdated >= 125000:
-    pyglet.app.exit()
-pyglet.clock.schedule(stop_after_time)
+  def stop_after_time(_dt):
+    if space.numParticlesUpdated >= 125000:
+      pyglet.app.exit()
+  pyglet.clock.schedule(stop_after_time)
 
-working = True
-def worker():
-  while working:
-    space.update_circle()
-   
-threads = [threading.Thread(target=worker, name=f"Thread-{i}") for i in range(4)]
-for thread in threads:
-  thread.start()
+  working = True
+  def worker():
+    while working:
+      space.update_circle()
+    
+  threads = [threading.Thread(target=worker, name=f"Thread-{i}") for i in range(t_num)]
+  for thread in threads:
+    thread.start()
 
-pyglet.app.run()
-working = False
-for thread in threads:
-  thread.join()
+  pyglet.app.run()
+  working = False
+  for thread in threads:
+    thread.join()
 
-plotter = InteractiveScatter(
-  space.xdata, 
-  space.distdata, 
-  space.devdata, 
-  labels=["Avg Distance from Center", "Avg Std Dev"]
-)
-plotter.plot()
+  plotter = InteractiveScatter(
+    space.xdata, 
+    space.distdata, 
+    space.devdata, 
+    labels=["Avg Distance from Center", "Avg Std Dev"]
+  )
+  plotter.plot()
 
-avg_dist = sum(space.distdata) / len(space.distdata) if space.distdata else 0
-avg_dev = sum(space.devdata) / len(space.devdata) if space.devdata else 0
-print(f"Average Distance: {avg_dist}")
-print(f"Average Std Dev: {avg_dev}")
+  avg_dist = sum(space.distdata) / len(space.distdata) if space.distdata else 0
+  avg_dev = sum(space.devdata) / len(space.devdata) if space.devdata else 0
+  print(f"Average Distance: {avg_dist}")
+  print(f"Average Std Dev: {avg_dev}")
 
-print("DONE")
+  print("DONE")
+
+if __name__ == "__main__":
+  run(t_num=1)
